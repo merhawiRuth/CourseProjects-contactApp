20fff89d6df528b0b611073838334c51baec789a
merhawi <merhawi.temnewo88@gmail.com>
2019-05-03 20:46:52 -0500

    able to filter data

--- src/App.js
+++ src/App.js
@@ -1,15 +1,46 @@
-import React, { Component } from 'react'
-import ListContacts from './ListContacts'
+import React, { Component } from 'react';
+import ListContacts from './ListContacts';
 
 class App extends Component {
-  
+  state = {
+    contacts: [
+      {
+        id: 'tyler',
+        name: 'Tyler McGinnis',
+        handle: '@tylermcginnis',
+        avatarURL: 'http://localhost:5001/tyler.jpg'
+      },
+      {
+        id: 'karen',
+        name: 'Karen Isgrigg',
+        handle: '@karen_isgrigg',
+        avatarURL: 'http://localhost:5001/karen.jpg'
+      },
+      {
+        id: 'richard',
+        name: 'Richard Kalehoff',
+        handle: '@richardkalehoff',
+        avatarURL: 'http://localhost:5001/richard.jpg'
+      }
+    ]
+  };
+  onRemove = contact => {
+    this.setState(currentContact => ({
+      contacts: currentContact.contacts.filter(c => {
+        return c.id !== contact.id;
+      })
+    }));
+  };
   render() {
     return (
       <div>
-          Hello world
+        <ListContacts
+          contacts={this.state.contacts}
+          onDeleteContact={this.onRemove}
+        />
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,5 +1,63 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
+class ListContacts extends Component {
+  static propTypes = {
+    contacts: PropTypes.array.isRequired,
+    onDeleteContact: PropTypes.func.isRequired
+  };
+  state = {
+    query: ''
+  };
 
+  updateQuery(query) {
+    this.setState({ query: query.trim() });
+  }
+  render() {
+    const { query } = this.state;
+    const { contacts, onDeleteContact } = this.props;
 
-export default ListContacts
+    const filterdData =
+      query === ''
+        ? contacts
+        : contacts.filter(c => {
+            return c.name.toLowerCase().includes(query.toLowerCase());
+          });
+    return (
+      <div className="list-contacts">
+        <div className="list-contacts-top">
+          <input
+            className="search-contacts"
+            type="text"
+            value={query}
+            onChange={event => this.updateQuery(event.target.value)}
+          />
+        </div>
+        <ol className="contact-list">
+          {filterdData.map(contact => (
+            <li key={contact.id} className="contact-list-item">
+              <div
+                className="contact-avatar"
+                style={{
+                  backgroundImage: `url(${contact.avatarURL})`
+                }}
+              />
+              <div className="contact-details">
+                <p>{contact.name}</p>
+                <p>{contact.handle}</p>
+              </div>
+              <button
+                onClick={() => onDeleteContact(contact)}
+                className="contact-remove"
+              >
+                Remove
+              </button>
+            </li>
+          ))}
+        </ol>
+      </div>
+    );
+  }
+}
+
+export default ListContacts;
